1d9f207ae164f984cd79c2b1bc88a1f4ab6a97f6
Malekpour99 <103121845+Malekpour99@users.noreply.github.com>
2023-10-11 05:18:04 +0330

    Updated blog's models.py
    Created Post model
    Added Meta class for ordering
    Overridden representation of a post object

--- blog/models.py
+++ blog/models.py
@@ -15,3 +15,12 @@ class Post(models.Model):
     published_date = models.DateTimeField()
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ['-created_date']
+        # ordering applied here is general, not just limited to the admin page
+        # you can change your presented class name 
+        #   using "verbose_name" and "verbose_name_plural"
+    
+    def __str__(self):
+        return f" {self.title} - {self.id}"
