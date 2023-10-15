5bbb4218feaf7eb2de83cd87e40025ef96fb6ab6
Malekpour99 <103121845+Malekpour99@users.noreply.github.com>
2023-10-15 15:07:50 +0330

    Updated blog's home_view
    Retrieving posts from Post model
    Filtering retrieved posts based on published_date and status
    Sending posts to the blog-home page

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,15 @@
+import datetime
 from django.shortcuts import render
-
+from blog.models import Post
 # Create your views here.
 
+
 def home_view(request):
-    return render(request, "blog/blog-home.html")
+    posts = Post.objects.filter(
+        published_date__lte=datetime.datetime.now(), status=True)
+    context = {'posts': posts}
+    return render(request, "blog/blog-home.html", context)
+
 
 def single_view(request):
     return render(request, "blog/blog-single.html")
