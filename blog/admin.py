0f38b7690c841da83ce6877992a9adfe314d0fd0
Malekpour99 <103121845+Malekpour99@users.noreply.github.com>
2023-10-16 06:07:24 +0330

    Updated blog's post admin panel
    Added author field to the list_display and filtering options

--- blog/admin.py
+++ blog/admin.py
@@ -9,9 +9,9 @@ class PostAdmin(admin.ModelAdmin):
     # Activates post filtering by date hierarchy
     empty_value_display = '-empty-'
     # Determines how empty values are displayed
-    list_display = ('title', 'counted_views', 'status',
+    list_display = ('title', 'author', 'counted_views', 'status',
                     'published_date', 'created_date')
-    list_filter = ('status',)
+    list_filter = ('status', 'author')
     # + "fields" can also be used to determine
     #   which fields to display on each data editing page
     # ordering = ['-created_date'] moved to Meta class
