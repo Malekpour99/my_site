64b7977edad7fabfac035a385153491089e1714c
Malekpour99 <103121845+Malekpour99@users.noreply.github.com>
2023-10-16 06:02:43 +0330

    Updated single-blog View
    Fixed a security issue for viewing unpublished posts using their ID

--- blog/views.py
+++ blog/views.py
@@ -12,7 +12,10 @@ def home_view(request):
 
 
 def single_view(request, pid):
-    post = get_object_or_404(Post, id=pid)
+    posts = Post.objects.filter(
+        published_date__lte=datetime.datetime.now(), status=True)
+    post = get_object_or_404(posts, id=pid)
+    # post = get_object_or_404(Post, id=pid) # this is unsafe because you can access not published posts by using ID
     post.counted_views += 1
     post.save()
     context = {'post': post}
