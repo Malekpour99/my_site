33879ac4efc223af10594e0893440d70978cae61
Malekpour99 <103121845+Malekpour99@users.noreply.github.com>
2023-10-21 05:49:11 +0330

    Blog's single_view new feature
    Added functionality for finding next & previous posts for each of blog's posts and injecting their data to the template

--- blog/views.py
+++ blog/views.py
@@ -18,5 +18,14 @@ def single_view(request, pid):
     # post = get_object_or_404(Post, id=pid) # this is unsafe because you can access not published posts by using ID
     post.counted_views += 1
     post.save()
-    context = {'post': post}
+    current_index = list(posts).index(post)
+    previous_post = None
+    next_post = None
+    if current_index - 1 >= 0:
+        next_post = list(posts)[current_index - 1]
+    if current_index + 1 < len(list(posts)):
+        previous_post = list(posts)[current_index + 1]
+    context = {'post': post,
+               'next_post': next_post,
+               'previous_post': previous_post}
     return render(request, "blog/blog-single.html", context)
