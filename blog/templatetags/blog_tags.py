6b713cb8488c77a1c536f6284a1f8c7fd83e291c
Malekpour99 <103121845+Malekpour99@users.noreply.github.com>
2023-11-04 06:01:35 +0330

    latest_posts inclusion tag
    Created latest_post inclusion tag for retrieving the latest published posts and showing them in the sidebar using blog-latest-posts template component

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,18 @@
+from django import template
+from blog.models import Post
+
+register = template.Library()
+
+
+# @register.simple_tag
+# def test_tag():
+#     pass
+
+# @register.filter
+# def test_filter():
+#     pass
+
+@register.inclusion_tag('blog/includes/blog-latest-posts.html')
+def latest_posts(num=3):
+    posts = Post.objects.filter(status=1).order_by('published_date')[:num]
+    return {'posts': posts}
