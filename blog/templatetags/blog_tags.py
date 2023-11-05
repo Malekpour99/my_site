a0a3e239052c56512ba1ab7fc790dce2661857e2
Malekpour99 <103121845+Malekpour99@users.noreply.github.com>
2023-11-05 06:05:08 +0330

    blog_tags - post_categories
    Created a custom inclusion tag for retrieving post categories and the number of posts related to them

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from blog.models import Post
+from blog.models import Post, Category
 
 register = template.Library()
 
@@ -14,5 +14,17 @@ register = template.Library()
 
 @register.inclusion_tag('blog/includes/blog-latest-posts.html')
 def latest_posts(num=3):
+    "Retrieving latest posts"
     posts = Post.objects.filter(status=1).order_by('-published_date')[:num]
     return {'posts': posts}
+
+
+@register.inclusion_tag('blog/includes/blog-categories.html')
+def post_categories():
+    "Retrieving categories and counting how many posts are related to them"
+    posts = Post.objects.filter(status=1)
+    categories = Category.objects.all()
+    cat_dict = {}
+    for category in categories:
+        cat_dict[category] = posts.filter(category=category).count()
+    return {'categories': cat_dict}
