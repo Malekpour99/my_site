7a6ae41085b165e6b7c5b6b4a833576dbda772e5
Malekpour99 <103121845+Malekpour99@users.noreply.github.com>
2023-11-07 05:57:06 +0330

    blog views - Category filtering
    Added new feature to the home_view for filtering posts based on their category

--- blog/views.py
+++ blog/views.py
@@ -4,9 +4,11 @@ from blog.models import Post
 # Create your views here.
 
 
-def home_view(request):
+def home_view(request, cat_name=None):
     posts = Post.objects.filter(
         published_date__lte=datetime.datetime.now(), status=True)
+    if cat_name:
+        posts= posts.filter(category__name=cat_name)
     context = {'posts': posts}
     return render(request, "blog/blog-home.html", context)
 
