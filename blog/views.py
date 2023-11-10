75c5d2e65289f9c01f54c1ab6083cb48c3575544
Malekpour99 <103121845+Malekpour99@users.noreply.github.com>
2023-11-10 07:26:28 +0330

    blog views - search functionality
    Added search feature for the blog posts based on their title and content.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 import datetime
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
+from django.db.models import Q
 # Create your views here.
 
 
@@ -8,9 +9,9 @@ def home_view(request, cat_name=None, author_username=None):
     posts = Post.objects.filter(
         published_date__lte=datetime.datetime.now(), status=True)
     if cat_name:
-        posts= posts.filter(category__name=cat_name)
+        posts = posts.filter(category__name=cat_name)
     if author_username:
-        posts= posts.filter(author__username=author_username)
+        posts = posts.filter(author__username=author_username)
     context = {'posts': posts}
     return render(request, "blog/blog-home.html", context)
 
@@ -33,3 +34,15 @@ def single_view(request, pid):
                'next_post': next_post,
                'previous_post': previous_post}
     return render(request, "blog/blog-single.html", context)
+
+
+def search_view(request):
+    # print(request.__dict__) # for seeing request contents
+    posts = Post.objects.filter(
+        published_date__lte=datetime.datetime.now(), status=True)
+    if request.method == 'GET':
+        # using python "warlus" option ( := ) for less code
+        if search_query := request.GET.get('search'):
+            posts = posts.filter(Q(content__contains=search_query) | Q(title__contains=search_query))
+    context = {'posts': posts}
+    return render(request, "blog/blog-home.html", context)
