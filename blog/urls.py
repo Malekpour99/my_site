5a0a4f4bf27361b1de0549acdde61b1f2cd53058
Malekpour99 <103121845+Malekpour99@users.noreply.github.com>
2023-11-25 11:35:22 +0330

    blog URLs - tag URL
    Added new URL path for filtering posts based on their tags in the home view

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("", home_view, name="home"),
     path("<int:pid>", single_view, name="single"),
     path("category/<str:cat_name>", home_view, name="category"),
+    path("tag/<str:tag_name>", home_view, name="tag"),
     path("author/<str:author_username>", home_view, name="author"),
     path("search/", search_view, name="search")
 ]
