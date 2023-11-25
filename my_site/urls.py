c0d74c2d9b08d1a3597a16b9e04b2b51cae820eb
Malekpour99 <103121845+Malekpour99@users.noreply.github.com>
2023-11-25 08:41:01 +0330

    my_site URLs - robots.txt
    Added robots.txt URL to the project paths in order to see the robots.txt content and rules for our project

--- my_site/urls.py
+++ my_site/urls.py
@@ -37,5 +37,6 @@ urlpatterns = [
         {"sitemaps": sitemaps},
         name="django.contrib.sitemaps.views.sitemap",
     ),
+    path("robots.txt", include("robots.urls"))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
   + static(settings. MEDIA_URL, document_root=settings.MEDIA_ROOT)
