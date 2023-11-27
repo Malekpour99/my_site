73d196fa76dd2a132981c0e659d2c258e51ff486
Amirhosein Malekpour <malekpour1999@gmail.com>
2023-11-27 22:25:44 +0330

    blog admin - summernote
    Added summernote to the PostAdmin and mentioned Post model 'content' field as a summernote field to use its features

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Post, Category
 # Register your models here.
 
 
 @admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
     date_hierarchy = 'created_date'
     # Activates post filtering by date hierarchy
     empty_value_display = '-empty-'
@@ -13,7 +15,7 @@ class PostAdmin(admin.ModelAdmin):
                     'published_date', 'created_date')
     list_filter = ('status', 'author')
     # + "fields" can also be used to determine
-    #   which fields to display on each data editing page
+    #   which fields to display on each data-editing page
     # ordering = ['-created_date'] moved to Meta class
     # Sets the ordering of your data list
     search_fields = ['title', 'content']
