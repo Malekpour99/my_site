aac6f2b1c3d09dcb563ad6165bbfbf7b37849722
Amirhosein Malekpour <malekpour1999@gmail.com>
2023-11-29 23:56:21 +0330

    blog admin - Comment Admin
    Registered Comment model in the admin panel also configured its displaying, filters and search fields

--- blog/admin.py
+++ blog/admin.py
@@ -1,25 +1,46 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import Post, Category
+from .models import Post, Category, Comment
+
 # Register your models here.
 
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-    summernote_fields = ('content',)
-    date_hierarchy = 'created_date'
+    summernote_fields = ("content",)
+    date_hierarchy = "created_date"
     # Activates post filtering by date hierarchy
-    empty_value_display = '-empty-'
+    empty_value_display = "-empty-"
     # Determines how empty values are displayed
-    list_display = ('title', 'author', 'counted_views', 'status',
-                    'published_date', 'created_date')
-    list_filter = ('status', 'author')
+    list_display = (
+        "title",
+        "author",
+        "counted_views",
+        "status",
+        "published_date",
+        "created_date",
+    )
+    list_filter = ("status", "author")
     # + "fields" can also be used to determine
     #   which fields to display on each data-editing page
     # ordering = ['-created_date'] moved to Meta class
     # Sets the ordering of your data list
-    search_fields = ['title', 'content']
+    search_fields = ["title", "content"]
     # Determines which fields will be searched for your search query
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    date_hierarchy = "created_date"
+    empty_value_display = "-empty-"
+    list_display = (
+        "name",
+        "post",
+        "approved",
+        "created_date",
+    )
+    list_filter = ("post", "approved")
+    search_fields = ["name", "post"]
 
 
 admin.site.register(Category)
