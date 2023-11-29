fa0a3f73c6ded1b457874b6151f12a66ed856716
Amirhosein Malekpour <malekpour1999@gmail.com>
2023-11-29 23:48:08 +0330

    blog models - Comment Model
    Created Comment Model for managing each post's comments

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from taggit.managers import TaggableManager
+
 # Create your models here.
 
 
@@ -9,14 +10,14 @@ class Category(models.Model):
     name = models.CharField(max_length=255)
 
     class Meta:
-        verbose_name_plural = 'categories'
+        verbose_name_plural = "categories"
 
     def __str__(self):
         return self.name
 
 
 class Post(models.Model):
-    image = models.ImageField(upload_to='blog/', default='blog/default.jpg')
+    image = models.ImageField(upload_to="blog/", default="blog/default.jpg")
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
@@ -29,7 +30,7 @@ class Post(models.Model):
     updated_date = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ['-created_date']
+        ordering = ["-created_date"]
         # ordering applied here is general, not just limited to the admin page
         # you can change your presented class name
         #   using "verbose_name" and "verbose_name_plural"
@@ -39,3 +40,14 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse("blog:single", kwargs={"pid": self.id})
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    approved = models.BooleanField(default=False)
