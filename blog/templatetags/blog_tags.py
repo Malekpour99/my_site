930ca9695e219dbadec2f161f927c7c903a051b3
Amirhosein Malekpour <malekpour1999@gmail.com>
2023-11-30 12:32:48 +0330

    custom blog tags - comments_count
    Created a custom simple_tag for counting each post's comments to be shown in the blog-home template

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,25 +1,27 @@
 from django import template
-from blog.models import Post, Category
+from blog.models import Post, Category, Comment
 
 register = template.Library()
 
-
-# @register.simple_tag
-# def test_tag():
-#     pass
 
 # @register.filter
 # def test_filter():
 #     pass
 
-@register.inclusion_tag('blog/includes/blog-latest-posts.html')
+
+@register.simple_tag
+def comments_count(pid):
+    return Comment.objects.filter(post=pid, approved=True).count()
+
+
+@register.inclusion_tag("blog/includes/blog-latest-posts.html")
 def latest_posts(num=3):
     "Retrieving latest posts"
-    posts = Post.objects.filter(status=1).order_by('-published_date')[:num]
-    return {'posts': posts}
+    posts = Post.objects.filter(status=1).order_by("-published_date")[:num]
+    return {"posts": posts}
 
 
-@register.inclusion_tag('blog/includes/blog-categories.html')
+@register.inclusion_tag("blog/includes/blog-categories.html")
 def post_categories():
     "Retrieving categories and counting how many posts are related to them"
     posts = Post.objects.filter(status=1)
@@ -27,4 +29,4 @@ def post_categories():
     cat_dict = {}
     for category in categories:
         cat_dict[category] = posts.filter(category=category).count()
-    return {'categories': cat_dict}
+    return {"categories": cat_dict}
