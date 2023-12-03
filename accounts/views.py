0ef8ce3b79c9febf9d440075fdead07602ead0ef
Amirhosein Malekpour <malekpour1999@gmail.com>
2023-12-03 05:29:20 +0330

    accounts views - login_view
    Created a basic view to tell that whether user in logged-in or not

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def login_view(request):
+    if request.user.is_authenticated:
+        msg = f"User is logged in as {request.user.username}"
+    else:
+        msg = "User in not logged in!"
+
+    return render(request, "accounts/login.html", {"msg": msg})
+
+
+# def logout_view(request):
+# pass
+
+
+def signin_view(request):
+    pass
