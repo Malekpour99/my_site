a7f4fc0f9da25f9e128b05979dd971e093a51825
Amirhosein Malekpour <malekpour1999@gmail.com>
2023-12-06 05:20:31 +0330

    blog models - Post login requirement
    Added a new field to the Post model called require_login in order to check if you are required to login in order to be able to see that post content

--- blog/models.py
+++ blog/models.py
@@ -25,6 +25,7 @@ class Post(models.Model):
     category = models.ManyToManyField(Category)
     counted_views = models.IntegerField(default=0)
     status = models.BooleanField()
+    require_login = models.BooleanField(default=False)
     published_date = models.DateTimeField()
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
