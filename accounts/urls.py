3762ee894aa531653178ce857ed14d28ce12b6d0
Amirhosein Malekpour <malekpour1999@gmail.com>
2023-12-12 22:04:20 +0330

    accounts URLs - forgotten password URLs
    Added Django built-in classes and URLs for users password reset process

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,11 @@
-from django.urls import path
-from .views import *
+from django.urls import path, reverse_lazy
+from .views import login_view, logout_view, signin_view, CustomPasswordResetConfirmView
+
+from django.contrib.auth.views import (
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetCompleteView,
+)
 
 app_name = "accounts"
 
@@ -7,4 +13,32 @@ urlpatterns = [
     path("login/", login_view, name="login"),
     path("logout", logout_view, name="logout"),
     path("signin", signin_view, name="signin"),
+    path(
+        "password-reset/",
+        PasswordResetView.as_view(
+            template_name="accounts/password_reset.html",
+            success_url=reverse_lazy("accounts:password_reset_done"),
+            email_template_name="accounts/password_reset_email.html",
+        ),
+        name="password_reset",
+    ),
+    path(
+        "password-reset/done/",
+        PasswordResetDoneView.as_view(
+            template_name="accounts/password_reset_done.html"
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        CustomPasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        PasswordResetCompleteView.as_view(
+            template_name="accounts/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
+    ),
 ]
