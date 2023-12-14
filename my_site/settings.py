7cdcf3eebe3a6d5ea2d9d01a9dbfd91576f4fdf3
Amirhosein Malekpour <malekpour1999@gmail.com>
2023-12-14 15:57:09 +0330

    project settings - development and production
    Moved development and production configuration settings from this file to their dedicated files, dev.py and prod.py

--- my_site/settings.py
+++ my_site/settings.py
@@ -16,18 +16,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ne0c2=(4*u1k5p+m)zxz46l8k1&gzwse7^n)j=!j7wgdr-m&$w"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -63,8 +51,6 @@ MIDDLEWARE = [
     "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
-# Sites
-SITE_ID = 2
 
 # Robots
 ROBOTS_USE_HOST = True
@@ -96,30 +82,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "my_site.wsgi.application"
-
-# Custom backend settings for authentication
-AUTHENTICATION_BACKENDS = ['accounts.backends.MyBackend']
-
-# Using console based Email message to reset password, instead of SMTP Service
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-# SMTP service configuration for sending emails
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'your_email@gmail.com'
-# EMAIL_HOST_PASSWORD = 'email_generated_app_password!'
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 
 
 # Password validation
@@ -157,35 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "static"
-
-STATICFILES_DIRS = [
-    BASE_DIR / "statics"
-]
-
 MEDIA_URL = "media/"
-MEDIA_ROOT = BASE_DIR / "media"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-# Django Debug Toolbar
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
-
-# Django summernote settings
-# X_FRAME_OPTIONS = "SAMEORIGIN"
-SUMMERNOTE_THEME = 'bs4'
-SUMMERNOTE_CONFIG = {
-    # Using SummernoteWidget - iframe mode, default
-    'iframe': True,
-
-    # You can put custom Summernote settings
-    'summernote': {
-        # As an example, using Summernote Air-mode
-        'airMode': False,
-    }
-}
