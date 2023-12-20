3487bc11b20dbcde4a194e822db5a9e3f54181bb
Amirhosein Malekpour <malekpour1999@gmail.com>
2023-12-20 06:14:20 +0330

    website URLs - coming soon path
    Added coming-soon template path to the website app URLs

--- website/urls.py
+++ website/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("", index_view, name="index"),
     path("contact", contact_view, name="contact"),
     path("about", about_view, name="about"),
-    path("newsletter", newsletter_view, name="newsletter")
+    path("newsletter", newsletter_view, name="newsletter"),
+    path("coming-soon", coming_soon_view, name="coming_soon"),
 ]
